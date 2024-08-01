618b5297bb29876f64f4bf4fa800f8cb409bb47a
Kasra Namiranian <114209244+KasraNamiranian@users.noreply.github.com>
2024-08-01 23:58:13 +0330

    Add files via upload

---
+++ text-base-quiz-game.py
@@ -0,0 +1,7 @@
+print('Welcome to AskPython Quiz')
+answer=input('Are you ready to play the Quiz ? (yes/no) :')
+score=0
+total_questions=3
+
+if answer.lower() == "yes":
+    answer = input("Question1: ")
